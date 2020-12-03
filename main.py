ed47a6d65e2158f6ff1350a0bbc3f54f74aa1887
Itay212121 <56035342+Itay212121@users.noreply.github.com>
2020-12-03 21:29:40 +0200

    added some features, and fixing time problems

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 from usedFunctions import IssmallerThanThePivot, returnMiddleValue, swap, returnMiddleValueint
 import time
-
+import json
 def sort(givenList):
 
+    firstElementIndex = 0
+    firstElement = givenList[0]
 
-    if givenList == []:
-        print(8)
 
-    firstElement = {
-        "index": 0,
-        "value": givenList[0]
-    }
+    lastElementIndex = len(givenList) - 1
+    lastElement =  givenList[len(givenList) - 1]
 
-    lastElement = {
-        "index": len(givenList) - 1,
-        "value": givenList[len(givenList) - 1]
-    }
 
-    midElement = {
-        "index": len(givenList) // 2,
-        "value": givenList[len(givenList) // 2]
-    }
+    midElementIndex = len(givenList) // 2
+    midElement = givenList[len(givenList) // 2]
+
 
 
 
@@ -46,43 +39,36 @@ def sort(givenList):
 
 
     sortedList = givenList #this was created so the list can be changed
-    pivot = returnMiddleValue(firstElement, midElement, lastElement)
+    pivotArray = returnMiddleValue([firstElement, firstElementIndex], [midElement, midElementIndex], [lastElement, lastElementIndex])
+    pivotValue = pivotArray[0]
+    pivotIndex = pivotArray[1]
 
-    if pivot["index"] != 0:
-        sortedList = swap(pivot, {"value": givenList[0], "index": 0}, sortedList)
+    if pivotIndex != 0:
+        sortedList = swap(pivotArray,  [givenList[0],  0], sortedList)
 
-    pivot = {"value": sortedList[0], "index": 0}
     borderIndex = 1
-    border = {"value": givenList[borderIndex], "index": borderIndex}
-    pointerIndex = 1
-    for p in range(1, len(givenList)):
+    borderValue = givenList[borderIndex]
+    for currentPointerIndex in range(1, len(givenList)):
 
-        pointer = {"value": givenList[p], "index": pointerIndex}
-
+        ThisIterPointerValue = givenList[currentPointerIndex]
 
 
-        IsSmaller = IssmallerThanThePivot(givenList, pivot, pointer["index"])
+
+        IsSmaller = IssmallerThanThePivot(givenList, pivotValue, ThisIterPointerValue)
         if IsSmaller:
 
-            sortedList = swap(border, pointer, sortedList)
+            sortedList = swap([borderValue, borderIndex], [ThisIterPointerValue, currentPointerIndex], sortedList)
             if borderIndex + 1 != len(givenList):
                 borderIndex += 1
 
-                border = {"value": givenList[borderIndex], "index": borderIndex}
+                borderValue = givenList[borderIndex]
 
-        pointerIndex += 1
+    sortedList = swap([pivotValue, pivotIndex], [borderValue, borderIndex], sortedList)
 
+    if borderIndex == 0:
+        borderIndex = 1
 
-
-    border = {"value": givenList[border["index"] - 1], "index": border["index"] - 1}
-    sortedList = swap(pivot, border, sortedList)
-
-
-
-    if border["index"] == 0:
-        border["index"] = 1
-
-    return sort(sortedList[:border["index"]]) + [border["value"]] + sort(sortedList[border["index"] + 1:])
+    return sort(sortedList[:borderIndex]) + [borderValue] + sort(sortedList[borderIndex + 1:])
 
 
 class Sort:
@@ -114,16 +100,48 @@ def returnStringThatSaysHowManyNumbers(givenList):
         stringOutput += (str(num) + ". " + str(numbersInThisArray[num]) + " times\n")
     return stringOutput
 
-sortClass = quick_sort()
+
+
+def returnAvrageSortTime(timeItTook):
+    with open("sortTime.json") as jsonFile:
+        data = json.load(jsonFile)
+        currentSortHistory = data["sortHistory"]
+        currentSortHistory = currentSortHistory.strip('][').split(', ')
+        if currentSortHistory == [""]:
+            currentSortHistory = []
+        if currentSortHistory == []:
+            currentSortHistory = [timeItTook]
+            with open("sortTime.json", "w") as jsonFile:
+                jsonFormat = {'sortHistory': str(currentSortHistory) }
+                json.dump(jsonFormat, jsonFile)
+            return timeItTook
+
+        currentSortHistory.append(timeItTook)
+        totalTime = 0
+        IndexOfTime = 0
+        betterSortHistory = []
+        for time in currentSortHistory:
+            betterSortHistory.append(time.replace('"', "").replace("'", ""))
+            totalTime += int(time.replace('"', "").replace("'", ""))
+            IndexOfTime += 1
+        avrageTime = totalTime / IndexOfTime
+    with open("sortTime.json", "w") as jsonFile:
+        jsonFormat = {'sortHistory': str(betterSortHistory)}
+
+        json.dump(jsonFormat, jsonFile)
+    return avrageTime
 
 
 def main():
-
+    IsWantToSeeList = "y" in input("Do u want to see a full list of the numbers with the numbers they show up? (y/n)")
     with  open("input.txt", "r") as inputFile:
-
+        MainClass = Sort()
+        sortClass = MainClass.quick_sort()
         startTimer = time.time()
         arrayOfLines = list(filter(lambda element:  element != "", inputFile.readlines()))
         arrayOfLines = list(map(lambda element: int(element.replace("\n", "")), arrayOfLines))
+        print("Sorting a list with length of " + str(len(arrayOfLines)))
+
         sortedList = sortClass.sort(arrayOfLines)
 
         outputFile = open("output.txt", "w")
@@ -133,11 +151,18 @@ def main():
 
         outputFile.write(outputString)
         afterTimer = time.time()
-        print("Time it took to quick sort algorithm: " + str((afterTimer - startTimer) * 1000) + "ms")
-        print(returnStringThatSaysHowManyNumbers(sortedList))
+        timeItTook = (afterTimer - startTimer) * 1000
+        avrageTime = str(returnAvrageSortTime(str(timeItTook)[:4]))
+
+        print("Time it took to quick sort algorithm: " + str(timeItTook)[:4] + "ms")
+        print("The current avrage time it takes: " + avrageTime[:4]  + "ms")
+
+        if IsWantToSeeList:
+            print(returnStringThatSaysHowManyNumbers(sortedList))
 
 
 if __name__ == '__main__':
+
     main()
 
 
